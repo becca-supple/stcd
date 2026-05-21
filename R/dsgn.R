4442bc31809f688615deea9c953ccf1893891fbe
Rebecca Supple <rbs9@st-andrews.ac.uk>
2026-05-21 14:59:38 +0100

    Fleshing out the dsgn functions and help file

--- R/dsgn.R
+++ R/dsgn.R
@@ -1,16 +1,35 @@
 #' Functions associated with 'dsgn' objects
 #'
-#' @param x A data.frame or coercible object
+#' @param x A data.frame or coercible object with shifted time series columns labeled 'VAR_tminus#'
 #'
 #' @returns A 'dsgn' object or TRUE/FALSE
 #' @export
 #'
 #' @examples
 #' design <- matrix(nrow = 2, ncol = 2)
-#' design <- dsgn(design)
-#' class(design)
+#' is.dsgn(design)
+#'
+#' # as.dsgn will throw an error if columns do not represent time series (i.e. if no column name contains 'tminus')
+#' try(design <- as.dsgn(design))
+#'
+#' colnames(design) <- c("var", "var_tminus1")
+#' design <- as.dsgn(design)
+#' is.dsgn(design)
 dsgn <- function(x) {
   class(x) <- c("dsgn", class(x))
   x
 }
-
+#' @export
+as.dsgn <- function(x){
+  if(!is.data.frame(x)){
+    x <- as.data.frame(x)
+  }
+  if(grepl("tminus", colnames(x))){
+    class(x) <- c("dsgn", class(x))}
+  else{stop("Provided object does not contain shifted time series columns")}
+  x
+}
+#' @export
+is.dsgn <- function(x){
+  "dsgn" %in% class(x)
+}
