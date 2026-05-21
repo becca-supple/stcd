f684d9d6f0f867733ef9c621278e0f5ff58b6af2
Rebecca Supple <rbs9@st-andrews.ac.uk>
2026-05-21 14:15:09 +0100

    Adding dsgn test function

--- R/dsgn.R
+++ R/dsgn.R
@@ -1,13 +1,16 @@
+#' Functions associated with 'dsgn' objects
+#'
+#' @param x A data.frame or coercible object
+#'
+#' @returns A 'dsgn' object or TRUE/FALSE
+#' @export
+#'
+#' @examples
+#' design <- matrix(nrow = 2, ncol = 2)
+#' design <- dsgn(design)
+#' class(design)
 dsgn <- function(x) {
   class(x) <- c("dsgn", class(x))
   x
 }
 
-as.dsgn <- function(x) {
-  class(x) <- c("dsgn", class(x))
-  x
-}
-
-is.dsgn <- function(x){
-  "dsgn" %in% class(x)
-}
